52cfc0c5ae1f72dd8dcec5e1c252e455d079df50
NIMISHA GEORGE <53266303+NimishaGeorge18@users.noreply.github.com>
2026-07-10 05:43:31 -0400

    Use environment variable for frontend API URL

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // All API calls go to our FastAPI backend
 const client = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8000',
   headers: { 'Content-Type': 'application/json' }
 });
 
